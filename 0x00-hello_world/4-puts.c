070a9833b9025fe292c915e2f2cbed0dde0095cd
RedietMillion <redietmillion14@gmail.com>
2022-04-08 03:55:47 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle ");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
+
