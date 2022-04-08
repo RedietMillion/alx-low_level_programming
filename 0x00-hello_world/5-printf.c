1ab7632e021614b8b5a6f48c0c0fc53ca4b33b5c
RedietMillion <redietmillion14@gmail.com>
2022-04-08 04:41:42 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main : print quots
+ *
+ *Return: zero, because the argument is is void
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
+
+
