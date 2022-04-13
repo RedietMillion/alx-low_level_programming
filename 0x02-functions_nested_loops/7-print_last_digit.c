47e45cc8cb9a360da5db43cd0a67047c517e7c49
RedietMillion <redietmillion14@gmail.com>
2022-04-13 15:15:24 -0700

    The last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,22 @@
  */
 
 int print_last_digit(int n)
+
 {
+	int last_digit;
+
 	if (n < 0)
-		return ((-1 * n) % 10);
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+
 	else
-		return (n % 10);
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+
 }
