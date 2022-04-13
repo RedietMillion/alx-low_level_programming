fb72a56e263da9f8c0223a30d5923306d7365685
RedietMillion <redietmillion14@gmail.com>
2022-04-13 14:00:14 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ *print_alphabet_x10 - print alphabet 10 times
+ *
+ *return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char alphabet = 'a';
+
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
