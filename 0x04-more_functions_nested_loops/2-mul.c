9aefdc014d1b7aee0828b125effeb2be3514cc17
RedietMillion <redietmillion14@gmail.com>
2022-04-16 14:46:52 +0300

    More function more nested loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,11 @@
 
 /**
  * mul - multiplies two integers*
- * @a: int to be multiplied to b*
+ * @a: int to be multiplied to b
  * @b: int to be multiplied to a
  *  Return: the result of the operation
  */
 int mul(int a, int b)
 {
-int c;
-c = a * b;
-return (c);
+return (a * b);
 }
